ae03a657b406dee41cf63776146a2ee9eea49ff3
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-08-13 11:56:44 -0400

    pre bower commit

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,4 +1,6 @@
 class SoundsController < ApplicationController
+  before_action :set_sound, except: [:create]
+
   def index
     @sounds = Sound.all
   end
@@ -6,4 +8,30 @@ class SoundsController < ApplicationController
   def show
     @sound = Sound.find(params[:id])
   end
+
+  def new
+    @sound = Sound.new
+  end
+
+  def create
+    @sound = Sound.new(sound_params)
+  end
+
+  def update
+    @sound.save
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def set_sound
+    @sound = Sound.find(params[:id])
+  end
+
+  def sound_params
+    params.require(:sound).permit(:code, :description)
+  end
+
 end
