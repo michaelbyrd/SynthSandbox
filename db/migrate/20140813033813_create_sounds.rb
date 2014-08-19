ee1552f2d765ede3854bc57dc58c8514649bdd73
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-08-19 11:14:21 -0400

    js issues

--- db/migrate/20140813033813_create_sounds.rb
+++ db/migrate/20140813033813_create_sounds.rb
@@ -3,6 +3,7 @@ class CreateSounds < ActiveRecord::Migration
     create_table :sounds do |t|
 
       t.string :title
+      t.boolean :public
       t.text :code
       t.text :description
       t.references :user
